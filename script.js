cbe5f976ca17c6d9e1a66313ba9105929fa92c27
ILikeCode1 <82778420+ILikeCode1@users.noreply.github.com>
2022-02-21 18:24:11 -0800

    Update script.js
    added reset button function

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ let percentButtons = document.querySelectorAll(".percent-list-items");
 let tipTotalAmount = document.querySelector(".tip-Total-Amount");
 let totalPersonAmount = document.querySelector(".total-person-amount");
 let resetButton = document.querySelector(".reset-button");
+
+
+
+// for reset button
+resetButton.addEventListener("click", reset);
+function reset() {
+  billTotalAmount.value = "";
+  peopleTotalAmount.value = "";
+  customPercentage.value = "";
+  tipTotalAmount.textContent = "$0.00";
+  totalPersonAmount.textContent = "$0.00";
+}
