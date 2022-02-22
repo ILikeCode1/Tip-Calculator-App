ad029a65e9f108ce00054630e794e9b0317e162d
ILikeCode1 <82778420+ILikeCode1@users.noreply.github.com>
2022-02-21 16:11:19 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let billTotalAmount = document.getElementById("bill-input");
+let peopleTotalAmount = document.getElementById("people-input");
+let customPercentage = document.getElementById("custom-input");
+
+let percentButtons = document.querySelectorAll(".percent-list-items");
+let tipTotalAmount = document.querySelector(".tip-Total-Amount");
+let totalPersonAmount = document.querySelector(".total-person-amount");
+let resetButton = document.querySelector(".reset-button");
