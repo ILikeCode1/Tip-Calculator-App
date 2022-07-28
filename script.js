7376db9426a72b87abff00778eeb9cf9d4892c50
ILikeCode1 <82778420+ILikeCode1@users.noreply.github.com>
2022-07-28 11:47:55 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,57 @@
-let billTotalAmount = document.getElementById("bill-input");
-let peopleTotalAmount = document.getElementById("people-input");
-let customPercentage = document.getElementById("custom-input");
+let billInput = document.getElementById("bill-input");
+let peopleInput = document.getElementById("people-input");
+let customInput = document.getElementById("custom-input");
 
 let percentButtons = document.querySelectorAll(".percent-list-items");
-let tipTotalAmount = document.querySelector(".tip-Total-Amount");
+let totalTipAmount = document.querySelector(".total-tip-amount");
 let totalPersonAmount = document.querySelector(".total-person-amount");
 let resetButton = document.querySelector(".reset-button");
 
+// Calculations
 
 
-// for reset button
+
+
+// Input Functions
+
+billInput.addEventListener("keyup", verifyBillInput);
+
+function verifyBillInput() {
+  if (billInput.value === "0" || billInput.value === "") {
+    billInput.style.outline = "2px solid hsl(15, 38%, 57%)";
+    errorMessage1.style.display = "block";
+  } else {
+    billInput.style.outline = "2px solid hsl(172, 31%, 51%)";
+    errorMessage1.style.display = "none";
+  }
+}
+
+peopleInput.addEventListener("keyup", verifyPeopleInput);
+
+function verifyPeopleInput() {
+  if (peopleInput.value === "0" || peopleInput.value === "") {
+    peopleInput.style.outline = "2px solid hsl(15, 38%, 57%)";
+    errorMessage2.style.display = "block";
+  } else {
+    peopleInput.style.outline = "2px solid hsl(172, 31%, 51%)";
+    errorMessage2.style.display = "none";
+  }
+}
+
+customInput.addEventListener("keyup", () => {
+  customInput.style.outline = "2px solid hsl(172, 31%, 51%)";
+  customInput.style.caretColor = "hsl(172, 31%, 51%)";
+});
+
+
+// Reset Button
+
 resetButton.addEventListener("click", reset);
+
 function reset() {
-  billTotalAmount.value = "";
-  peopleTotalAmount.value = "";
-  customPercentage.value = "";
-  tipTotalAmount.textContent = "$0.00";
-  totalPersonAmount.textContent = "$0.00";
+  billInput.value = "";
+  peopleInput.value = "";
+  customInput.value = "";
+  tipTotalAmount.innerHTML = "$0.00";
+  totalPersonAmount.innerHTML = "$0.00";
 }
